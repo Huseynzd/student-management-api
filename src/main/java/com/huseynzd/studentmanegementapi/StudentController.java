df72e013dbbf2de6eca9dfec4332b8454a5f895e
Huseynzd <nhuseynzad11@gmail.com>
2026-06-01 00:14:01 +0400

    Add Student and GET /students endpoint

---
+++ src/main/java/com/huseynzd/studentmanegementapi/StudentController.java
@@ -0,0 +1,17 @@
+package com.huseynzd.studentmanegementapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private List<Student> students = new ArrayList<>();
+
+    public List<Student> getAllStudents(){
+        return students;
+    }
+}
