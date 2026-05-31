ceaa411a845839b5995044f3767b838a2ddac7e0
Huseynzd <nhuseynzad11@gmail.com>
2026-05-31 23:20:04 +0400

    Initial Spring Boot project

---
+++ src/main/java/com/huseynzd/studentmanegementapi/StudentManegementApiApplication.java
@@ -0,0 +1,13 @@
+package com.huseynzd.studentmanegementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManegementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManegementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huseynzd/studentmanegementapi/StudentManegementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huseynzd.studentmanegementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManegementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
